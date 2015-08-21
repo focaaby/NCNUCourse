f0f01f0160318963b40f27c3ac86a434067a7e59
Jerry Wang <focaaby@gmail.com>
2015-08-21 19:00:53 +0800

    fixed bug: save worong db in post

--- js/control.js
+++ js/control.js
@@ -3,57 +3,54 @@ var app = angular.module("courseApp", ['ui.bootstrap' ,'firebase']);
 app.controller("CourseCtrl", function($scope, $firebaseObject, $firebaseAuth) {
 	var ref = new Firebase("https://ncnu-course.firebaseio.com/course1");
 	$scope.authObj = $firebaseAuth(ref);
-	var obj = $firebaseObject(ref);
-	obj.$bindTo($scope, "courses");
-	// get arrary index
+	$scope.courses = $firebaseObject(ref);
 
-	$scope.score = function(index) {
-		$scope.myModalLabel = $scope.courses[index].cname;
-		var postsRef = ref.child(index).child('posts');	
-		var postsObj = $firebaseObject(postsRef);
-		postsObj.$bindTo($scope, "posts");
-
-
-		$scope.addPost = function (recommendValue, messagesValue) {
-			var authData = ref.getAuth();
-			if(authData == null){
-				alert("請先登入Facebook喲!!");
-			} else{
-				if (!messagesValue) {
-					messagesValue = '';
-				};
-				var dateValue = Date.now();
-				postsRef.push({
-					userid: authData.facebook.id, 
-					username: authData.facebook.displayName,
-					img: authData.facebook.profileImageURL, 
-					recommend: recommendValue,
-					messages: messagesValue, 
-					date: dateValue
-				});
-				$scope.course.post = [];
+	var key;
+	$scope.score = function(index) {		
+		key = index;
+		$scope.myModalLabel = $scope.courses[key].cname;
+		var postsRef = ref.child(key).child('posts');	
+		$scope.posts = $firebaseObject(postsRef);
+		var tureNum = 0;
+		var falseNum = 0;
+		angular.forEach($scope.posts, function(value, key) {
+			if (value.recommend == 1) {	
+				tureNum++;
+				console.log($scope.posts + ', value' + value);
+			} else {
+				falseNum++;	
+				console.log($scope.posts + ', value' + value);
 			}
-		}
+		});
+		console.log( 'tureNum:' + tureNum  + ', falseNum:' + falseNum);
+	}
 
-		$scope.getRecommendTure = function () {
-			var tureNum = 0;
-			angular.forEach($scope.courses[index].posts, function(value, key) {
-				if (value.recommend == 1) {	
-					tureNum++;
-				}
+	$scope.addPost = function (recommendValue, messagesValue) {
+		//console.log('key: ' + key);
+		var postsRef = ref.child(key).child('posts');	
+		$scope.posts = $firebaseObject(postsRef);
+
+		var authData = ref.getAuth();
+		if(authData == null){
+			alert("請先登入Facebook喲!!");
+		} else{
+			if (!messagesValue) {
+				messagesValue = '';
+			};
+			var dateValue = Date.now();
+			postsRef.push({
+				userid: authData.facebook.id, 
+				username: authData.facebook.displayName,
+				img: authData.facebook.profileImageURL, 
+				recommend: recommendValue,
+				messages: messagesValue, 
+				date: dateValue
 			});
-			return tureNum;
-		}
-		$scope.getRecommendFalse = function () {
-			var falseNum = 0;
-			angular.forEach($scope.courses[index].posts, function(value, key) {
-				if (value.recommend == 0) {
-					falseNum++;
-				}
-			});
-			return falseNum;
+			$scope.recommend = "";
+			$scope.message = "";
 		}
 	}
+	
 
 	$scope.bug = function() {
 		$('#bugModal').modal('show');
